71d45f039612d4198548934d7029d183acc4d4a7
heveidndksjd <kayoonkim@naver.com>
2025-05-07 15:08:41 +0900

    feat: subtract and divide API 구현

--- src/main/java/com/likelion/session/service/calculator/CalculatorService.java
+++ src/main/java/com/likelion/session/service/calculator/CalculatorService.java
@@ -6,10 +6,22 @@ import org.springframework.stereotype.Service;
 public class CalculatorService {
     public int add(int number1, int number2) {
         return number1 + number2;
+    }
 
+    public int subtract(int number1, int number2) {
+        return number1 - number2;
     }
 
     public int multiply(int number1, int number2) {
         return number1 * number2;
     }
+
+    public int divide(int number1, int number2) {
+        if (number2 == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return number1 / number2;
+    }
+
+
 }
